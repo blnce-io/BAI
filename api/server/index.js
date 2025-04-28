db58ff32803b83a716de8646bf997716a5acbd99
Eugene Saigak <eugene@getbalance.com>
2025-04-28 10:07:40 +0300

    feat: completions api

--- api/server/index.js
+++ api/server/index.js
@@ -109,7 +109,7 @@ const startServer = async () => {
   app.use('/api/agents', routes.agents);
   app.use('/api/banner', routes.banner);
   app.use('/api/bedrock', routes.bedrock);
-
+  app.use('/api/completions', routes.completions);
   app.use('/api/tags', routes.tags);
 
   app.use((req, res) => {
